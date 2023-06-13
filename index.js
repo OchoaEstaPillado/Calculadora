4d3ce1133fe6892ff03b8d5b94d34883fae62be4
OchoaEstaPillado <125396394+OchoaEstaPillado@users.noreply.github.com>
2023-06-13 18:07:52 +0200

    Create index.js

---
+++ index.js
@@ -0,0 +1,42 @@
+function dis(val) {
+    document.getElementById("result").value += val;
+}
+
+function del() {
+    let x = document.getElementById("result").value.slice(0, -1);
+    document.getElementById("result").value = x;
+}
+
+function myFunction(event) {
+    if (event.key == '0' || event.key == '1' 
+        || event.key == '2' || event.key == '3'
+        || event.key == '4' || event.key == '5' 
+        || event.key == '6' || event.key == '7'
+        || event.key == '8' || event.key == '9' 
+        || event.key == '+' || event.key == '-'
+        || event.key == '*' || event.key == '/')
+        document.getElementById("result").value += event.key;
+}
+
+var cal = document.getElementById("buttons");
+cal.onkeyup = function (event) {
+    if (event.keyCode === 13) {
+        console.log("Enter");
+        let x = document.getElementById("result").value
+        console.log(x);
+        solve();
+    }
+}
+
+// Function that evaluates the digit and return result
+function solve() {
+    let x = document.getElementById("result").value;
+    let y = math.eval(x);
+    document.getElementById("result").value = y;
+    console.log(y);
+}
+
+// Function that clear the display
+function clr() {
+    document.getElementById("result").value = "";
+}
